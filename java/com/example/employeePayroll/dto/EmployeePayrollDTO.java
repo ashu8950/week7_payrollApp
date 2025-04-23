0a34d96f24d323bfd00cd2de6fa9ebb951bbe0c4
Ashu <shabsingh961@gmail.com>
2025-04-23 19:08:52 +0530

    [ASHU]Add. UC2 -> Ensure validations is done on the Payroll DTO

--- java/com/example/employeePayroll/dto/EmployeePayrollDTO.java
+++ java/com/example/employeePayroll/dto/EmployeePayrollDTO.java
@@ -1,21 +1,47 @@
 package com.example.employeePayroll.dto;
 
+import java.time.LocalDate;
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PastOrPresent;
 import jakarta.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.ToString;
 
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public @ToString class EmployeePayrollDTO {
-	@Pattern(regexp = "^[A-Z]{1}[A-Za-z\\s]{2,}$",message="Employee name invalid")
-	public String name;
-	
-	@Min(value = 500 ,message="Min wage should be more than 500")
-	public long salary;
-	
-	public String gender;
-	public String startDate;
-	public String note;
-	public String profilePic;
-	public List<String>deparment;
+
+    @Pattern(regexp = "^[A-Z]{1}[A-Za-z\\s]{2,}$", message = "Employee name invalid")
+    public String name;
+
+    @Min(value = 500, message = "Min wage should be more than 500")
+    public long salary;
+
+    @Pattern(regexp = "(?i)male|female", message = "Gender must be 'male' or 'female'")
+    public String gender;
+
+    @JsonFormat(pattern = "dd MMM yyyy")
+    @NotNull(message = "Start date should not be null")
+    @PastOrPresent(message = "Start date should be past or present")
+    public LocalDate startDate;
+
+    @NotBlank(message = "Note cannot be blank")
+    public String note;
+
+    @NotBlank(message = "Profile pic cannot be empty")
+    public String profilePic;
+
+    @NotNull(message = "Department should not be empty")
+    public List<String> department;
 }
