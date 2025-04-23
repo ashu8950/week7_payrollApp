536e2281df59cdb58e26863364491560685a6130
Ashu <shabsingh961@gmail.com>
2025-04-23 19:16:17 +0530

    [ASHU]Add.UC4 -> handling one to many mapping cascading using(element Collection)

--- java/com/example/employeePayroll/model/Employee.java
+++ java/com/example/employeePayroll/model/Employee.java
@@ -1,12 +1,20 @@
 package com.example.employeePayroll.model;
+
+import jakarta.persistence.CollectionTable;
+import jakarta.persistence.Column;
+import jakarta.persistence.ElementCollection;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.Table;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
 import java.util.List;
 
 import com.example.employeePayroll.dto.EmployeePayrollDTO;
@@ -17,27 +25,37 @@ import com.example.employeePayroll.dto.EmployeePayrollDTO;
 @Entity
 @Table(name = "employees")
 public class Employee {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int employeeId;
+    @Column(name = "id")
+    private int id;
 
+    @Column(name = "name")
     private String name;
+
     private long salary;
     private String gender;
-    private String startDate;
+    private LocalDate startDate;
     private String note;
     private String profilePic;
-    private List<String>department;
-    
+
+    @ElementCollection
+    @CollectionTable(
+        name = "employee_departments",
+        joinColumns = @JoinColumn(name = "employee_id")
+    )
+    @Column(name = "department")
+    private List<String> department;
+
     public Employee(int empId, EmployeePayrollDTO empPayrollDTO) {
-    	this.employeeId =empId;
-    	this.name = empPayrollDTO.name;
-    	this.salary = empPayrollDTO.salary;
-    	this.gender = empPayrollDTO.gender;
-    	this.startDate = empPayrollDTO.startDate;
-    	this.note = empPayrollDTO.note;
-    	this.profilePic = empPayrollDTO.profilePic;
-    	this.department = empPayrollDTO.deparment;
+        this.id = empId;
+        this.name = empPayrollDTO.name;
+        this.salary = empPayrollDTO.salary;
+        this.gender = empPayrollDTO.gender;
+        this.startDate = empPayrollDTO.startDate;
+        this.note = empPayrollDTO.note;
+        this.profilePic = empPayrollDTO.profilePic;
+        this.department = empPayrollDTO.department;
     }
-    
 }
